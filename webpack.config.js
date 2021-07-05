29139cea48567f864202f256d52569445cf1f801
codewithwaleed <codewithwaleed@gmail.com>
2021-07-05 16:49:38 +0500

    eslint rules updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,24 @@
-import { resolve } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-export const mode = 'development';
-export const entry = resolve(__dirname, 'src', 'index.js');
-export const output = {
-  // the output of the webpack build will be in /dist directory
-  path: resolve(__dirname, 'dist'),
-  // the filename of the JS bundle will be bundle.js
-  filename: 'bundle.js'
-};
-export const module = {
-  rules: [
-    {
-      // for any file with a suffix of js or jsx
-      test: /\.jsx?$/,
-      // ignore transpiling JavaScript from node_modules as it should be that state
-      exclude: /node_modules/,
-      // use the babel-loader for transpiling JavaScript to a suitable format
-      loader: 'babel-loader',
-      options: {
-        // attach the presets to the loader (most projects use .babelrc file instead)
-        presets: ['@babel/preset-env', '@babel/preset-react']
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
       }
-    }
-  ]
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })]
 };
-export const plugins = [new HtmlWebpackPlugin({ template: resolve(__dirname, 'src', 'index.html') })];
